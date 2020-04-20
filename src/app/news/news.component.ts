b21d56aaf42967a852b7e00eddf39b84006c3417
love8820 <quanghmph08578@fpt.edu.vn>
2020-04-21 00:26:10 +0700

    1 file modified

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { New } from '../New';
+import { NewService } from '../new.service' 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
+  news: New[];
+  constructor(
+    private newService: NewService ) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit(): void {
+      this.getNews();
+      // this.products = this.productService.getProducts();
+    }
+    getNews(){
+      this.newService.getNews().subscribe(data =>{
+        this.news =data; 
+      })
+    }
+ 
 }
